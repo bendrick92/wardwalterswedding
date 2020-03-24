d800ade9cb0a8d624529fe740da2abc050ba2de1
Ben Walters <ben@Bens-Mac-mini-2.local>
2020-03-23 20:19:38 -0500

    Updated because fuck Corona

--- scripts.js
+++ scripts.js
@@ -1,4 +1,9 @@
 $(document).ready(function () {
+    var subscribeForm = $('#subscribe-form');
+    var subscribeError = $('#subscribe-error');
+    var subscribeSuccess = $('#subscribe-success');
+    var subscribeEmail = $('#subscribe-email');
+
     var apiEndpoint = 'https://jukebox-api-free.azurewebsites.net/api/Suggestion';
     //var apiEndpoint = 'http://localhost';
 
@@ -88,8 +93,6 @@ $(document).ready(function () {
                 'Recaptcha': $('#g-recaptcha-response').val()
             });
 
-            console.log(json);
-
             $.ajax({
                 url: apiEndpoint,
                 type: 'POST',
@@ -107,6 +110,26 @@ $(document).ready(function () {
                     showJukeboxError();
                 }
             });
+        }
+    });
+
+    $('#subscribe-email').on('keypress', function (e) {
+        subscribeEmail.css('border', 'none');
+        subscribeError.hide();
+        subscribeSuccess.hide();
+    });
+
+    $('#subscribe-form').on('submit', function (e) {   
+        var form = document.getElementById('subscribe-form');
+
+        e.preventDefault();
+        
+        var valid = trySubscribe();
+
+        if (valid) {
+            var data = new FormData(form);
+
+            $.ajax(form.method, form.action, data, subscribeSuccess, subscribeError);
         }
     });
 
@@ -172,5 +195,33 @@ $(document).ready(function () {
         jukeboxItem.append('<td>' + submitter + '</td>');
         
         jukeboxTable.find('tbody').append(jukeboxItem);
+    }
+
+    function trySubscribe() {
+        if (validateEmail(subscribeEmail.val())) {
+            return true;
+        }
+        else {
+            subscribeEmail.css('border', '1px solid #F00');
+            subscribeError.text('Please enter a valid email');
+            subscribeError.show();
+            return false;
+        }
+
+        function validateEmail(email) {
+            var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            return re.test(email.toLowerCase());
+        }
+    }
+
+    function subscribeSuccess() {
+        subscribeForm.reset();
+        subscribeSuccess.text('Submitted!');
+        subscribeSuccess.show();
+    }
+
+    function subscribeError() {
+        subscribeError.text('There was an error - please try again!');
+        subscribeError.show();
     }
 });
